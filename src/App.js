105fbc38ffa95ce7f865fb119c87d99e51549cbd
Lucca Saraiva <luccasarfer@gmail.com>
2022-09-08 16:46:31 -0300

    Nascimento

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
+          target="_blank" 
           rel="noopener noreferrer"
         >
           Learn React
