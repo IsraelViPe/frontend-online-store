acba7eaef07a6aa741c4c4e53e7f1dc5bad0050c
CalebeLAR <ca6ele@gmail.com>
2022-09-08 15:31:22 -0400

    requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,14 @@
 export async function getCategories() {
-  // Implemente aqui
+  const data = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const results = await data.json();
+  return results;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(CATEGORY_ID, QUERY) {
+  const data = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${CATEGORY_ID}&q=${QUERY}`);
+  const results = await data.json();
+  console.log(results);
+  return results;
 }
 
 export async function getProductById() {
