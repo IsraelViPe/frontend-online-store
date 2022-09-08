5a4526e4c131295e76614ba98587cfd9197f7c80
Lucca Saraiva <luccasarfer@gmail.com>
2022-09-08 18:42:25 -0300

    requisito 3 abatido

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <div>
+        <div id="pesquisa">
           <input
             type="text"
           />
-        </div>
-        <p data-testid="home-initial-message">
+          <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
-
-        </p>
+          </p>
+        </div>
+        <Link data-testid="shopping-cart-button" to="/cart">Carrinho</Link>
       </div>
     );
   }
