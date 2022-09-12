a791dd38d6fc876d96e8a3b85f96f5f144adb9ec
IsraelPereira <pereira.israel2070@gmail.com>
2022-09-12 20:48:39 -0300

    adiciona lógica do botão de adicionar ao carrinho da pagina de detalhes

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { getProductById } from '../services/api';
+
 // import ProductDetails from '../components/ProductDetails';
 
 export default class ProductPage extends React.Component {
   state = {
     renderDetailProduct: {},
+    loadCart: JSON.parse(localStorage.getItem('cart')) || [],
+    updateCart: JSON.parse(localStorage.getItem('cart')) || [],
+    counterItens: 0,
   };
 
   async componentDidMount() {
@@ -18,13 +22,41 @@ export default class ProductPage extends React.Component {
   puxaProduto = async () => {
     const { match: { params: { id } } } = this.props;
     const detailsProduct = await getProductById(id);
-    console.log(detailsProduct);
     return detailsProduct;
+  };
+
+  handleAddCartFromDetailPage = ({ target: { id } }) => {
+    const { loadCart, renderDetailProduct } = this.state;
+    const { title, price, thumbnail, id: idProduct } = renderDetailProduct;
+    const productAmount = loadCart.find(({ idList }) => idList === id).addCount;
+    const nonRepeatedItens = loadCart.filter((element) => element.idList !== id);
+    const repeatedItens = loadCart.filter((element) => element.idList === id);
+    if (repeatedItens.length > 0) {
+      this.setState((prevState) => ({
+        counterItens: prevState.counterItens + 1,
+        updateCart: [...nonRepeatedItens],
+      }), () => {
+        this.setState((prevState) => ({
+          updateCart: [...prevState.updateCart, {
+            thumbnail,
+            price,
+            title,
+            idList: idProduct,
+            addCount: productAmount ? prevState.counterItens + productAmount : 2,
+          }],
+        }), this.saveProductsCart);
+      });
+    }
+  };
+
+  saveProductsCart = () => {
+    const { updateCart } = this.state;
+    localStorage.setItem('cart', JSON.stringify(updateCart));
   };
 
   render() {
     const { renderDetailProduct } = this.state;
-    const { title, price, thumbnail } = renderDetailProduct;
+    const { title, price, thumbnail, id } = renderDetailProduct;
 
     return (
       <div>
@@ -52,6 +84,14 @@ export default class ProductPage extends React.Component {
         >
           Carrinho
         </Link>
+        <button
+          id={ id }
+          type="button"
+          data-testid="shopping-cart-button"
+          onClick={ this.handleAddCartFromDetailPage }
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }
